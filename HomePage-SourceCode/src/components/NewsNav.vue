1978f67ba4823401d77402d697b4444b83da4a45
zitao <liuzitao0123@foxmail.com>
2022-04-06 14:50:19 +0800

    添加了用于展示的图片幻灯片以及标题

---
+++ HomePage-SourceCode/src/components/NewsNav.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <ImgList :images="pics"></ImgList>
+    <NewsTitleList :titles="titles"></NewsTitleList>
+  </div>
+</template>
+
+<script>
+import ImgList from '@/components/ImgList'
+import NewsTitleList from '@/components/NewsTitleList'
+
+export default {
+  name: 'NewsNav',
+  components: {NewsTitleList, ImgList},
+  props: {
+    images: {
+      type: Array,
+      required: true
+    },
+    titles: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    pics () {
+      return this.images.map((src, index) => {
+        return {src, alt: '' + index + '.jpg'}
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  display: flex;
+}
+</style>
---
+++ HomePage-SourceCode/src/components/NewsTitleList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="titles">
+    <h1 id="header">News</h1>
+    <hr>
+    <div id="TitlesContainer" role="list">
+      <div v-for="(title, index) of titles" :key="index" role="listitem">
+        <div class="dot"></div>
+        <a href="#" id="title">{{ title }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewsTitleList',
+  props: ['titles']
+}
+</script>
+
+<style scoped>
+
+h1, hr {
+  margin: 0;
+}
+
+div#titles {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 50%;
+}
+
+div#TitlesContainer {
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+  height: 100%;
+  justify-content: space-between;
+}
+
+div#TitlesContainer > div{
+  height: auto;
+  display: flex;
+  align-items: center;
+}
+
+a#title {
+  line-height: 30px;
+  text-decoration: none;
+}
+
+div.dot {
+  width: 8px;
+  height: 8px;
+  border-radius: 4px;
+  background-color: black;
+  display: inline-block;
+  margin-right: 10px;
+}
+</style>
