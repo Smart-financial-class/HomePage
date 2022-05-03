7519ba0462749053bbeecae57fe5751baaa83932
HDU_Nbsp <1808347356@qq.com>
2022-05-03 20:44:42 +0800

    引入了parallax包，添加了preview界面（开发中）

--- HomePage-SourceCode/src/router/index.js
+++ HomePage-SourceCode/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '../views/HomePage'
 import ApiDoc from '../views/ApiDoc'
+import Preview from '@/views/Preview'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/apidoc',
       name: 'ApiDoc',
       component: ApiDoc
+    },
+    {
+      path: '/preview',
+      name: 'Preview',
+      component: Preview
     }
   ]
 })
---
+++ HomePage-SourceCode/src/views/Preview.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="body" id="scene" :style="{ height: style.height }">
+    <div id="textBox" data-depth="0.3">
+      <div id="mainTitle">智能财务</div>
+      <div id="subTitle">智在杭电</div>
+    </div>
+    <div id="frontIMG" class="imgBox" data-depth="0.6">
+      <img src="../assets/HDU/building.png" alt="三鼎问天" class="images" />
+    </div>
+    <div id="backIMG" class="imgBox" data-depth="0.2">
+      <img src="../assets/HDU/building4.png" alt="4教" class="images" />
+    </div>
+    <!-- <div id="sky" data-depth="0.1">
+      <img src="../assets/HDU/sky.jpg" alt="sky">
+    </div> -->
+  </div>
+</template>
+
+<script>
+import Parallax from 'parallax-js'
+
+export default {
+  name: 'Preview',
+  data () {
+    return {
+      style: {
+        height: window.innerHeight + 'px'
+      }
+    }
+  },
+  mounted () {
+    window.onresize = () => {
+      this.style.height = window.innerHeight + 'px'
+    }
+    let scene = document.getElementById('scene')
+    // eslint-disable-next-line
+    let parallaxInstance = new Parallax(scene)
+  }
+}
+</script>
+
+<style scoped>
+.body {
+  overflow: hidden;
+}
+
+.images {
+  width: 100%;
+}
+
+.imgBox {
+  position: fixed;
+  width: 100%;
+  height: 80%;
+  margin-left: 20%;
+  margin-top: 10%;
+}
+
+#frontIMG {
+  z-index: 2;
+  margin-top: 0;
+  margin-left: 0;
+  width: 110%;
+}
+
+#backIMG {
+  z-index: 1;
+  width: 80%;
+}
+
+#mainTitle {
+  font-size: 120px;
+  font-weight: 1200;
+}
+
+/* #sky {
+  width: 110%;
+} */
+</style>
